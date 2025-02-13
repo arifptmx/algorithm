d1f832703af42acc7c0c51690634853b6a2c4619
arifptmx <arifptmx@gmail.com>
2025-02-13 11:45:46 +0700

    bubble sort

---
+++ sorting_algorithm/bubble_sort.js
@@ -0,0 +1,18 @@
+const bubbleSort = (arr) => {
+    let n = arr.length;
+    let swapped;
+
+    for (let i = 0; i < n - 1; i++) {
+        swapped = false;
+        for (let j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+                swapped = true
+            }
+        }
+        if (!swapped) break
+    }
+    return arr
+}
+
+console.log(bubbleSort([5,3,8,4,2]))
